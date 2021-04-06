c74f04404a1882617314eb1278417879be210997
Michael Wooley <wm.wooley@gmail.com>
2021-04-05 22:43:54 -0400

    clean up

--- main.tf
+++ main.tf
@@ -16,53 +16,6 @@ provider "aws" {
 }
 
 
-// // https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/kms_key
-// resource "aws_kms_key" "bucket_sse" {
-//   description              = "This key is used to encrypt bucket objects"
-//   enable_key_rotation      = true
-//   is_enabled               = true
-//   customer_master_key_spec = "SYMMETRIC_DEFAULT"
-//   key_usage                = "ENCRYPT_DECRYPT"
-//   policy                   = <<POLICY
-// {
-//     "Version": "2012-10-17",
-//     "Statement": [
-//         {
-//             "Sid": "Enable IAM User Permissions",
-//             "Effect": "Allow",
-//             "Principal": {
-//                 "AWS": "arn:aws:iam::${var.aws_account_id}:root"
-//             },
-//             "Action": "kms:*",
-//             "Resource": "*"
-//         },
-//         {
-//             "Sid": "Allow access through S3 for all principals in the account that are authorized to use S3",
-//             "Effect": "Allow",
-//             "Principal": {
-//                 "AWS": "*"
-//             },
-//             "Action": [
-//                 "kms:Encrypt",
-//                 "kms:Decrypt",
-//                 "kms:ReEncrypt*",
-//                 "kms:GenerateDataKey*",
-//                 "kms:DescribeKey"
-//             ],
-//             "Resource": "*",
-//             "Condition": {
-//                 "StringEquals": {
-//                     "kms:CallerAccount": "${var.aws_account_id}",
-//                     "kms:ViaService": "s3.${var.aws_region}.amazonaws.com"
-//                 }
-//             }
-//         }
-//     ]
-// }
-// POLICY
-// }
-
-
 resource "aws_cloudfront_origin_access_identity" "sse_oai" {
   comment = "Access the bucket"
 }
@@ -73,7 +26,6 @@ resource "aws_s3_bucket" "test_bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        // kms_master_key_id = aws_kms_key.bucket_sse.arn
         sse_algorithm     = "AES256"
       }
     }
@@ -113,13 +65,6 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     domain_name = "${aws_s3_bucket.test_bucket.bucket}.s3.${aws_s3_bucket.test_bucket.region}.amazonaws.com"
     origin_id   = "s3-${aws_s3_bucket.test_bucket.id}"
     origin_path = ""
-
-    // custom_origin_config {
-    //   https_port             = 443
-    //   http_port              = 80
-    //   origin_protocol_policy = "https-only"
-    //   origin_ssl_protocols   = ["TLSv1.2"]
-    // }
 
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.sse_oai.cloudfront_access_identity_path
